978bcd7f5f0a192aa6fb7c477cd65dc64f982cfc
DanielFrogel <daniel.frogel@gmail.com>
2024-04-26 12:27:36 -0300

    Limpeza código/organização, DocStrings e adicionador algumas melhorias

--- app.py
+++ app.py
@@ -12,36 +12,69 @@ import json
 import pystray
 from pystray import MenuItem as item
 from PIL import Image
+import sys
+import ctypes
+from PyQt5 import QtWidgets
+from PyQt5 import QtWinExtras
+ctypes.windll.shell32.ExtractIconW.restype = ctypes.c_size_t
+from tkinter import filedialog
 
-# Função para encerrar a aplicação
-def exit_action(icon, item): 
+# Para Pegar o ícone do executável
+def displayIcon(path: str, index=0):
+    hicon = ctypes.windll.shell32.ExtractIconW(0, path, index)
+    qpixmap = QtWinExtras.QtWin.fromHICON(hicon)
+    ctypes.windll.user32.DestroyIcon(ctypes.c_void_p(hicon))
+    name = os.path.expandvars('%appdata%\\zipador_tdm\\icone.ico')
+    qpixmap.save(name, 'ICO')
+
+# Encerrar a aplicação pelo Systray
+def fechar_app(icon, item): 
     icon.visible = False 
     os._exit(0)    
 
+# Abre Configuração pelo Systray
+def selecionar_pasta():
+    pasta_monitoramento = filedialog.askdirectory()
+    if (pasta_monitoramento) != '':
+        arquivo_log(f'Seleciona Nova Pasta de Monitoramento: {pasta_monitoramento}')
+        with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), 'w') as arquivo_json:
+            arquivo_json.write(f'''[
+    {{
+        "caminho_pasta": "{pasta_monitoramento}"
+    }}
+]''')
+        reiniciar_app()    
+
+# Abrir o arquivo de Log pelo Systray
 def abrir_arquivo_log():
     os.startfile(os.path.expandvars('%appdata%\\zipador_tdm\\Log.log'))    
 
-# Criar ícone na bandeja do sistema
+# Reiniciar Aplicação
+def reiniciar_app():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
+# Criar ícone no Systray
 def create_systray():
-    # Carregar uma imagem para o ícone
+    # Tentar carregar uma imagem para o ícone do systray, caso não encontre cria uma em branco
     try:
-        image = Image.open(os.path.expandvars('%appdata%\\zipador_tdm\\icone.png'))
+        image = Image.open(os.path.expandvars('%appdata%\\zipador_tdm\\icone.ico'))
     except FileNotFoundError:
-        blank_image = Image.new('RGBA', (16, 16), (0, 0, 0, 0))
-        blank_image.save(os.path.expandvars('%appdata%\\zipador_tdm\\icone.png'), 'PNG')
-        image = Image.open(os.path.expandvars('%appdata%\\zipador_tdm\\icone.png')) 
+        app = QtWidgets.QApplication(sys.argv)  
+        displayIcon(sys.executable)
+        image = Image.open(os.path.expandvars('%appdata%\\zipador_tdm\\icone.ico')) 
 
-    # Criar o ícone na bandeja do sistema
-    icon = pystray.Icon('zipador_tdm', image, 'Zipador de TDM')
+    icon = pystray.Icon('zipador_tdm', image, f'Zipador de TDM - Monitorante Pasta: {ler_arquivo_json()}')
 
-    # Adicionar item de menu para sair
     abrir_log = item('Abrir Log', abrir_arquivo_log)
-    exit_item = item('Encerrar', exit_action)
-    icon.menu = (abrir_log, exit_item,)
+    selecionar = item('Selecionar Pasta Monitoramento', selecionar_pasta)
+    reiniciar = item('Reiniciar', reiniciar_app)
+    fechar = item('Encerrar', fechar_app)
+    icon.menu = (abrir_log, selecionar, pystray.Menu.SEPARATOR, reiniciar, fechar,)
 
-    # Exibir o ícone
     icon.run()
 
+# Classe para verificar se aplicação já está aberta
 class SingleInstance:
     def __init__(self, name):
         self.mutexname = name
@@ -57,18 +90,20 @@ class SingleInstance:
         if self.mutex:
             win32api.CloseHandle(self.mutex)
 
-# Função para compactar arquivo em um arquivo zip
+# Compactar arquivo em um arquivo zip
 def zip_arquivo(arquivo):
     nome_zip = os.path.splitext(arquivo)[0] + '.zip'
     with zipfile.ZipFile(nome_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
         zipf.write(arquivo, os.path.basename(arquivo))
 
+# Adicionar texto no arquivo de Log
 def arquivo_log(texto):
     log = os.path.expandvars('%appdata%\\zipador_tdm\\Log.log')
     with open(log, 'a') as arquivo:
         arquivo.write(f'{(datetime.date.today()).strftime("%d/%m/%Y")} - {(datetime.datetime.now()).strftime("%H:%M")}\n{texto}\n\n') 
         arquivo.close()   
             
+# Lê o arquivo txt e altera o nome para o padrão compacta e depois exclui o txt            
 def modifica_arquivo_tdm(arquivo_txt):
     caminho = Path(arquivo_txt) 
     leitura = []
@@ -84,8 +119,8 @@ def modifica_arquivo_tdm(arquivo_txt):
             novo_nome = f'{caminho.parent}\\{leitura[1][58:67]}_{leitura[1][3:23]}_TDM.txt'                                                    
             
             if (leitura[0].startswith('E01')) and novo_nome != '__TDM.txt':                                                 
-                os.rename(arquivo_txt, novo_nome)  # Renomeia o arquivo
-                zip_arquivo(novo_nome)  # Compacta o arquivo renomeado            
+                os.rename(arquivo_txt, novo_nome) 
+                zip_arquivo(novo_nome)            
                 os.remove(novo_nome)
                 arquivo_log(f'Criado arquivo com Sucesso: {novo_nome}')
             else:
@@ -97,7 +132,7 @@ def modifica_arquivo_tdm(arquivo_txt):
             if e.errno == 13:
                 return False   
             
-# Classe para manipular os eventos do sistema de arquivos
+# Classe que monitora quando têm um arquivo.txt novo na pasta
 class MonitorarPasta(FileSystemEventHandler):
     def __init__(self):
         super().__init__()
@@ -105,62 +140,66 @@ class MonitorarPasta(FileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             return
-        elif event.src_path.endswith('.txt'): #and event.event_type == 'modified':  # Se o arquivo criado for um txt
-            time.sleep(1)  # Pequeno atraso para garantir que o arquivo esteja completamente escrito no disco
-            # Verificar se o arquivo foi modificado após o último processamento
-            #if os.path.exists(event.src_path) and os.stat(event.src_path).st_mtime > self.ultimo_processamento:
+        elif event.src_path.endswith('.txt'):
+            time.sleep(1) 
             try:
                 while not modifica_arquivo_tdm(event.src_path):
                     time.sleep(10)
             except Exception as e:
                 arquivo_log(f'Erro ao criar arquivo: {e}')
 
+# Tenta ler o caminho da pasta de monitoramento da arquivo settings.json e inverte a \ para / para evitir erro no json
+# Caso não encontre ele pega o caminho do executável
+def ler_arquivo_json():
+    try:
+        with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), encoding='ANSI', mode='r') as arquivo_json:
+            settings_json = arquivo_json.read()
+            settings_json = settings_json.replace('\\','/')  
+            arquivo_json.close()
+        with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), 'w') as arquivo_json:
+            arquivo_json.write(settings_json)
+            arquivo_json.close()
+        with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), mode='r') as arquivo_json:                                
+            settings = json.load(arquivo_json)
+            arquivo_json.close()  
+            return str(settings[0]['caminho_pasta']).replace('/','\\')
+    except FileNotFoundError:
+            arquivo_log(f'Arquivo de Configuração \'settings.json\' não Encontrado.\nCriado Novo Arquivo de Configuração!')
+            with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), 'w') as arquivo_json:
+                arquivo_json.write(f'''[
+    {{
+        "caminho_pasta": "{os.getcwd()}"
+    }}
+]''')
+            return ler_arquivo_json()                      
 
 if __name__ == "__main__":
-    # Defina um nome único para o seu aplicativo
-    app_name = "zipador_tdm"    
+    # Nome único para a aplicação, para não executar 2 ao mesmo tempo
+    app_name = "zipador_tdm" 
+    instance = SingleInstance(app_name) 
+       
+    # Cria a pasta no %appdata%
+    if not os.path.exists(os.path.expandvars('%appdata%\\zipador_tdm\\')):
+        os.mkdir(os.path.expandvars('%appdata%\\zipador_tdm\\'))
     
-    caminho_pasta = ''
-    
-    instance = SingleInstance(app_name)
-    
+    # Verificar se a aplicação já está aberta
     if instance.already_running():
         instance.cleanup()
     else:        
-        # Caminho para a pasta que você deseja monitorar
-        try:
-            with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), encoding='ANSI', mode='r') as arquivo_json:
-                settings_json = arquivo_json.read()
-                settings_json = settings_json.replace('\\','/')  
-                arquivo_json.close()
-                with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), 'w') as arquivo_json:
-                    arquivo_json.write(settings_json)
-                    arquivo_json.close()
-                with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), mode='r') as arquivo_json:                                
-                    settings = json.load(arquivo_json)
-                    caminho_pasta = str(settings[0]['caminho_pasta']).replace('/','\\')
-                    arquivo_json.close()                    
-        except FileNotFoundError:
-            arquivo_log(f'Arquivo de Configuração \'settings.json\' não Encontrado.\nCriado Novo Arquivo de Configuração Zerado!')
-            with open (str(os.path.expandvars('%appdata%\\zipador_tdm\\settings.json')), 'w') as arquivo_json:
-                arquivo_json.write('''[{
-  "caminho_pasta": ""
-}]''')
-            instance.cleanup()  
-            os._exit(0) 
-        
-        observer = Observer()
-        # Inicializar o observador  
+             
+        # Inicia o monitoramento
+        observer = Observer()          
         
         try:
-            observer.schedule(MonitorarPasta(), path=caminho_pasta, recursive=True)
+            observer.schedule(MonitorarPasta(), path=ler_arquivo_json(), recursive=True)
             observer.start()
         except FileNotFoundError:
             arquivo_log(f'Pasta de monitoramento não encontrada!')
             instance.cleanup()
             observer.stop() 
             os._exit(0)           
-            
+        
+        #Cria o icone no Systray    
         create_systray()                   
     
         try:
